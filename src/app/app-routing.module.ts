e2a2a31bfee8600a53e5ff31a7777846547b7966
OFS1\koyi.raghavarao <koyi.raghavarao@object-frontier.com>
2019-03-13 13:00:55 +0530

    Added  admin feature module and established routing link between app module and admin module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent, children: [
-    { path: 'login', component: LoginComponent, children: [] },    
-    { path: 'signup', component: SignUpComponent, children: [] }
-  ] }
-
+  { path: '', component: HomeComponent, children: []},
+  { path: 'login', component: LoginComponent, children: [] },    
+  { path: 'signup', component: SignUpComponent, children: [] },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminModule' }
 ];
 
 @NgModule({
